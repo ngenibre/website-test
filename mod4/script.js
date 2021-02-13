f40e8be86121b106b7cce4a8775e6bf15a559208
Nicolas <nicolas@collation.net>
2021-02-13 17:28:10 -0600

    module4

---
+++ mod4/script.js
@@ -0,0 +1,14 @@
+(function () {
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (var name in names) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter === 'j') {
+        byeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+    
+})();
